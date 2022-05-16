3d8253b44e6f51946eca83eb7eda130800c466c5
yasaman <yasamanbarati81@gmail.com>
2022-05-16 13:39:27 +0430

    #Create React-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+// import {Routes ,Route} from 'react-router-dom';
+import './components/pages/style.css'
+
+
+function App() {
+  return (
+    <Fragment>
+    </Fragment>
+  );
+}
+
+export default App;
